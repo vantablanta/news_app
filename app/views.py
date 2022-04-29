7c7dd989e485dee7e3703120e802d48864e550e4
vanta blanta <vantablanta@gmail.com>
2022-04-29 16:34:16 +0300

    news page added in views

--- app/views.py
+++ app/views.py
@@ -7,3 +7,8 @@ from .request import get_news
 def index():
     response = get_news()
     return render_template("index.html",data = response)
+
+@app.route("/news")
+def news():
+    response = get_news()
+    return render_template("news.html",data = response)
