c1604bd23cb05dbe6b73a4e9ee5ab70dd3fad6d3
vanta blanta <vantablanta@gmail.com>
2022-04-29 16:12:24 +0300

    news card holder added

--- app/__init__.py
+++ app/__init__.py
@@ -2,13 +2,11 @@
 from flask import Flask
 from .config import DevConfig
 
+
 # configurations
 app = Flask(__name__, instance_relative_config = True)
 app.config.from_object(DevConfig)
 app.config.from_pyfile('config.py')
 
-# initialize extensions 
-from flask_bootstrap import Bootstrap
-bootstrap  = Bootstrap(app)
 
 from app import views
