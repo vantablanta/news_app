0b2f34bebca334cefaa3c39117b5c96578c4f749
vanta blanta <vantablanta@gmail.com>
2022-05-02 12:45:49 +0300

    class test cases added

---
+++ app/tests/source_test.py
@@ -0,0 +1,20 @@
+from models import source
+import unittest
+
+
+Source = source.Source
+
+class SourceTest(unittest.TestCase):
+    '''Testace to test the Source Class'''
+
+    def setUp(self):
+        '''
+        Set up method that will run before every Test
+        '''
+        self.new_source = Source()
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_source, Source))
+
+
+if __name__ == '__main__':
+    unittest.main()
