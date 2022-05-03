81c98644d21f1b1931edfa5c04fd015ad1bd83ce
vanta blanta <vantablanta@gmail.com>
2022-05-03 11:54:41 +0300

    environment path added

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,12 @@
 # initialize the application 
+import os
 from flask import Flask
 from .config import DevConfig
 
 
 
 # configurations
-app = Flask(__name__, instance_relative_config = True)
+app = Flask(__name__, instance_path=os.path.join(os.path.abspath(os.curdir), 'instance'),instance_relative_config = True)
 app.config.from_object(DevConfig)
 app.config.from_pyfile('config.py')
 
